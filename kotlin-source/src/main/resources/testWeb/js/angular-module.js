eb778aaf3386b9fb8b1e5070aef5cda54dc92291
raymogg <raymondmogg@gmail.com>
2018-04-10 11:19:48 +1000

    UI Upgrades

--- kotlin-source/src/main/resources/testWeb/js/angular-module.js
+++ kotlin-source/src/main/resources/testWeb/js/angular-module.js
@@ -31,6 +31,9 @@ app.controller('DemoAppController', function($http, $location, $uibModal) {
     const apiBaseURL = "/api/example/";
     //Variables for TD app
     var activeTDs = [];
+    var currentOffers = [];
+    //Get nodes name and display in browser
+    displayNodeName();
 
     // First we pull the TD's from the api -> these are displayed on the home screen
     $http.get("/api/term_deposits/deposits").then(function (response) {
@@ -39,6 +42,7 @@ app.controller('DemoAppController', function($http, $location, $uibModal) {
             String(element.endDate) + ", Internal State: " + String(element.internalState));
             loaded(activeTDs);});
         });
+
     function loaded(array) {
         var ul = document.createElement('ul');
         ul.setAttribute('id','proList');
@@ -56,6 +60,32 @@ app.controller('DemoAppController', function($http, $location, $uibModal) {
             li.innerHTML=li.innerHTML + element;
                  });
     }
+
+    //Next we pull all offers this node has and display these
+    $http.get("/api/term_deposits/offers").then(function (response) {
+                response.data.offers.forEach(function (element) {
+                currentOffers.push("From: " + String(element.issuingInstitute) + ", Interest: " + String(element.interest) + ", Duration: " +
+                String(element.duration) + ", Valid Until: " + String(element.validTill));
+                loadedOffers(currentOffers);});
+            });
+
+        function loadedOffers(array) {
+            var ul = document.createElement('ul');
+            ul.setAttribute('id','proList');
+            var t, tt;
+            document.getElementById('currentOffers').innerHTML = "<h3>Current Offers</h3>";
+            document.getElementById('currentOffers').appendChild(ul);
+            array.forEach(function (element, index, arr) {
+                var li = document.createElement('li');
+                li.setAttribute('class','item');
+
+                ul.appendChild(li);
+
+                t = document.createTextNode(element);
+
+                li.innerHTML=li.innerHTML + element;
+                     });
+        }
 
     //OnClick methods for each button -> used for loading new pages for TD functionality
     demoApp.issueTD = () => {
@@ -86,6 +116,32 @@ app.controller('DemoAppController', function($http, $location, $uibModal) {
 
         modalInstance.result.then(() => {}, () => {});
     };
+
+    function displayNodeName() {
+        //Make the API Call then change the title
+        var url = "/api/example/me"
+        $http.get(url).then(function (response) {
+            var title = document.getElementById("title").innerHTML = "TD Cordapp: " + extractOrganisationName(response.data.me);
+        });
+    }
+
+    function extractOrganisationName(partyString) {
+                var actualName = "";
+                var seenEqual = 0;
+                //Extract and return the party name by parsing a string
+                for(var i = 0; i < partyString.length; i++) {
+                    if (partyString.charAt(i) == '=') {
+                        //First instance of seeing = is right after O, so from now till next L we append these to the name
+                        if (!seenEqual) {
+                            seenEqual = 1;
+                        }
+                    } else if (partyString.charAt(i) == ',') {//stop parsing the name
+                        return actualName;
+                    } else if (seenEqual) {
+                        actualName += partyString.charAt(i);
+                    }
+                }
+            }
 
 });
 
